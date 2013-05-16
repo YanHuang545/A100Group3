5b0ccb69dd1dcd169a9913830a34037c711984a6
YanHuang <Chuck545@gmail.com>
2013-05-16 00:54:24 -0400

    job posting
    form completed

--- Yan-Job posting form/process_registration.php
+++ Yan-Job posting form/process_registration.php
@@ -8,17 +8,20 @@
   <body>
     <h1>Thank You</h1>
 
-    <p>Thank you for registering. Here is the information you submitted:</p>
+    <p>Thank you for your time. Here is the job information you submitted:</p>
 
     <dl>
-      <dt>First name</dt><dd><?php echo $_POST["firstName"]?></dd>
-      <dt>Last name</dt><dd><?php echo $_POST["lastName"]?></dd>
-      <dt>Password</dt><dd><?php echo $_POST["password1"]?></dd>
-      <dt>Retyped password</dt><dd><?php echo $_POST["password2"]?></dd>
-      <dt>Gender</dt><dd><?php echo $_POST["gender"]?></dd>
-      <dt>Favorite widget</dt><dd><?php echo $_POST["favoriteWidget"]?></dd>
-      <dt>Do you want to receive our newsletter?</dt><dd><?php echo $_POST["newsletter"]?></dd>
-      <dt>Comments</dt><dd><?php echo $_POST["comments"]?></dd>
+      <dt>Job Title</dt><dd><?php echo $_POST["jobTitle"]?></dd>
+      <dt>Job Type</dt><dd><?php echo $_POST["jobType"]?></dd>
+      <dt>Location</dt><dd><?php echo $_POST["location"]?></dd>
+      <dt>Company Name</dt><dd><?php echo $_POST["companyName"]?></dd>
+      <dt>Company Description</dt><dd><?php echo $_POST["companyDescription"]?></dd>
+      <dt>Job Description</dt><dd><?php echo $_POST["jobDescription"]?></dd>
+      <dt>Skills Required</dt><dd><?php echo $_POST["skills"]?></dd>
+      <dt>Posting Date</dt><dd><?php echo $_POST["postingDate"]?></dd>
+      <dt>Contact Person</dt><dd><?php echo $_POST["contactPerson"]?></dd>
+      <dt>E-mail</dt><dd><?php echo $_POST["email"]?></dd>
+      
     </dl>
 
   </body>
