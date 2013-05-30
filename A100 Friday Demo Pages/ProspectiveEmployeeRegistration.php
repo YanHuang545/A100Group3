6f53558b636b7a29d1054de49a4e59dee565e242
John C <cerilloj2@owls.southernct.edu>
2013-05-30 16:11:22 -0400

    Employee Registration and Login Works
    I changed the table name, field names so you'll have to change them in
    your database to run this. Remember, the password field must be  of type
    varchar(32). Going to work on the employer side now

--- A100 Friday Demo Pages/ProspectiveEmployeeRegistration.php
+++ A100 Friday Demo Pages/ProspectiveEmployeeRegistration.php
@@ -108,27 +108,27 @@
     <dl>
       <dt>First name:</dt><dd><?php echo $_POST["first_name"]?></dd>
       <dt>Last name:</dt><dd><?php echo $_POST["last_name"]?></dd>
-      <dt>Email:</dt><dd><?php echo $_POST["city"]?></dd>
-      <dt>Phone:</dt><dd><?php echo $_POST["occupation"]?></dd>
+      <dt>City:</dt><dd><?php echo $_POST["city"]?></dd>
+      <dt>Occupation:</dt><dd><?php echo $_POST["occupation"]?></dd>
 	  <dt>User ID:</dt><dd><?php echo $_POST["user_name"]?></dd>
     </dl>
 
 <?php
 
-$sql = "INSERT INTO users (first_name, last_name, city, occupation, employer, user_name, password)
-VALUES(:first_name, :last_name,:city,:occupation, :employer, :user_name, :password)"; //not presently sure how to handle the files
+$sql = "INSERT INTO Employees (FirstName, LastName, City, Occupation, Employer, UserName, Password)
+VALUES(:FirstName, :LastName,:City,:Occupation, :Employer, :UserName, :Password)"; //not presently sure how to handle the files
 
 $hashedpassword=md5($_POST["pass"]);
 
 try {
 $st = $conn->prepare( $sql );
-$st->bindValue( ":first_name", $_POST["first_name"], PDO::PARAM_STR );
-$st->bindValue( ":last_name", $_POST["last_name"], PDO::PARAM_STR );
-$st->bindValue( ":city", $_POST["city"], PDO::PARAM_STR );
-$st->bindValue( ":occupation", $_POST["occupation"], PDO::PARAM_STR );
-$st->bindValue( ":employer", $_POST["employer"], PDO::PARAM_STR );
-$st->bindValue( ":user_name", $_POST["user_name"], PDO::PARAM_STR );
-$st->bindValue( ":password", $hashedpassword, PDO::PARAM_STR );
+$st->bindValue( ":FirstName", $_POST["first_name"], PDO::PARAM_STR );
+$st->bindValue( ":LastName", $_POST["last_name"], PDO::PARAM_STR );
+$st->bindValue( ":City", $_POST["city"], PDO::PARAM_STR );
+$st->bindValue( ":Occupation", $_POST["occupation"], PDO::PARAM_STR );
+$st->bindValue( ":Employer", $_POST["employer"], PDO::PARAM_STR );
+$st->bindValue( ":UserName", $_POST["user_name"], PDO::PARAM_STR );
+$st->bindValue( ":Password", $hashedpassword, PDO::PARAM_STR );
 $st->execute();
 } catch ( PDOException $e ) {
 echo "Query failed: " . $e->getMessage();
